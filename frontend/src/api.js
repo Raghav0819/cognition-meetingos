d1c4a34284f562c466b5776dcb388d0dfd4d7767
Raghav0819 <raghav.khandelwal.23cse@bmu.edu.in>
2026-07-31 02:26:56 +0530

    build issue solved

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { auth } from '../firebase'
+import { auth } from './firebase'
 
 const api = axios.create({ baseURL: import.meta.env.VITE_API_URL || 'https://cognition-meetingos.onrender.com' })
 
